dc7ea09782df8269c4d0c279bbeba8ff190267f6
Miguel Kantun <miguelkh21@gmail.com>
2023-06-22 11:03:04 -0600

    Se agregan funciones piloto para recoleccion de datos

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,19 +1,25 @@
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+//Esta primera parte es para la validación de los datos
+
+const formulario = document.getElementById('formulario'); //Esto llama el formulario
+const inputs = document.querySelectorAll('#formulario input'); //Con el querySelectoAll llama todos los imputs dentro del ID especificado
 
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, //Debe empezar con letra/numero seguido de un @letras/numeros y contener sí o sí un "." con el  \. se logra esto y terminar con letras/numeros
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
+
+//Esta parte setea todos los campos en false, se utiliza para la función del botón enviar
 const campos = {
 	nombre: false,
 	password: false,
 	email: false,
 	telefono: false
 }
+
+
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
@@ -38,14 +44,11 @@ const validarFormulario = (e) => {
 
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
-		document.getElementById(`grupo_${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo_${campo}`).classList.add('formulario__grupo-correcto');
 
 		document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
 		campos[campo] = true;
 	} else {
-		document.getElementById(`grupo_${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo_${campo}`).classList.remove('formulario__grupo-correcto');
+
 		document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
 		campos[campo] = false;
 	}
@@ -56,13 +59,9 @@ const validarPassword2 = () => {
 	const inputPassword2 = document.getElementById('floatingConfirm');
 
 	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo_password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo_password2`).classList.remove('formulario__grupo-correcto');
 		document.querySelector(`#grupo_password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
 		campos['password'] = false;
 	} else {
-		document.getElementById(`grupo_password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo_password2`).classList.add('formulario__grupo-correcto');
 		document.querySelector(`#grupo_password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
 		campos['password'] = true;
 	}
@@ -87,5 +86,31 @@ formulario.addEventListener('submit', (e) => {
 
 	} else {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+		}, 2000);
 	}
 });
+
+
+//Esta parte es para crear los usuarios 
+
+var nombreUsuario = document.getElementById("floatingName");
+var telefonoUsuario = document.getElementById("floatingTel");
+var emailUsuario = document.getElementById("floatingInput");
+var passwordUsuario = document.getElementById("floatingPassword");
+
+const botonEnviar = document.getElementById("boton")
+
+function guardarUsuario(){
+
+	console.log("Esta funcionando la funcion");
+
+
+
+	console.log(usuario1);
+
+}
+
+
+botonEnviar.addEventListener("click", guardarUsuario);
