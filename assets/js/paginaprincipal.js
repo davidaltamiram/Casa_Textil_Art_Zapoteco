fef29c16b904e5b2b8022a7d207bc67018dd3419
Les <les.floresbae@gmail.com>
2023-06-21 18:41:35 -0600

    Se agrega página principal html, css y js. Además algunas imagenes necesarias

---
+++ assets/js/paginaprincipal.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:3,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:true
+        },
+        1000:{
+            items:6,
+            nav:true,
+            
+        }
+    }
+})
